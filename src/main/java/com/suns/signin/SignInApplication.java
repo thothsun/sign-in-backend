05585686ae541967e5ba37b668577f6484ddaa40
sunshuai <438819781@qq.com>
2019-05-21 16:45:42 +0800

    Init

---
+++ src/main/java/com/suns/signin/SignInApplication.java
@@ -0,0 +1,13 @@
+package com.suns.signin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignInApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suns/signin/SignInApplicationTests.java
@@ -0,0 +1,16 @@
+package com.suns.signin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SignInApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
