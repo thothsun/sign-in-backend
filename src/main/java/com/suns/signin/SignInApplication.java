fc944d7c19ac0830cab4935b05fab38df0a6cbff
sunshuai <438819781@qq.com>
2019-05-23 17:21:39 +0800

    添加前端页面

--- src/main/java/com/suns/signin/SignInApplication.java
+++ src/main/java/com/suns/signin/SignInApplication.java
@@ -4,6 +4,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
---
+++ src/main/java/com/suns/signin/frontend/FrontEndController.java
@@ -0,0 +1,18 @@
+package com.suns.signin.frontend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by sunshuai on 2019/5/23
+ */
+@Controller
+public class FrontEndController {
+
+    @GetMapping(path = "/")
+    public String login(){
+        return "login";
+    }
+
+}
